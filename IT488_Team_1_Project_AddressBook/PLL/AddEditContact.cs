9f8b26e47f0a1fe5a83f84679483345b91f09be0
dandrzej80 <dandrzej80@gmail.com>
2021-11-08 17:56:50 -0500

    some input validation

--- IT488_Team_1_Project_AddressBook/PLL/AddEditContact.cs
+++ IT488_Team_1_Project_AddressBook/PLL/AddEditContact.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Text;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace IT488_Team_1_Project_AddressBook.PLL
@@ -19,7 +16,52 @@ namespace IT488_Team_1_Project_AddressBook.PLL
         #region Button Events
         private void saveButton_Click(object sender, EventArgs e)
         {
+            bool errors = false;
 
+            //Data Validation
+            #region Data Validation
+            if (!firstNameTextBox.Text.All(c => Char.IsLetter(c)))
+            {
+                firstNameLabel.ForeColor = Color.Red;
+                errors = true;
+            }
+            else firstNameLabel.ForeColor = Color.Black;
+            if (!lastNameTextBox.Text.All(c => Char.IsLetter(c)))
+            {
+                lastNameLabel.ForeColor = Color.Red;
+                errors = true;
+            }
+            else lastNameLabel.ForeColor = Color.Black;
+            if (!cityTextBox.Text.All(c => Char.IsLetter(c)))
+            {
+                cityLabel.ForeColor = Color.Red;
+                errors = true;
+            }
+            else cityLabel.ForeColor = Color.Black;
+            if (!stateTextBox.Text.All(c => Char.IsLetter(c)) || stateTextBox.Text.Length > 2)
+            {
+                stateLabel.ForeColor = Color.Red;
+                errors = true;
+            }
+            else stateLabel.ForeColor = Color.Black;
+            if (!zipTextBox.Text.All(c => Char.IsNumber(c)) || stateTextBox.Text.Length > 5)
+            {
+                zipLabel.ForeColor = Color.Red;
+                errors = true;
+            }
+            else zipLabel.ForeColor = Color.Black;
+
+            #endregion
+
+            //Check for errors and display message dialog.
+            if (errors)
+            {
+                MessageBox.Show("Input Errors", "Please correct input errors.");
+            }
+            else
+            {
+                this.Close();
+            }
         }
         private void cancelButton_Click(object sender, EventArgs e)
         {
